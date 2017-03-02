2529b287e6b63c2b57029a89b7fef4dee370bf2a
limed <limed@mozilla.com>
2017-03-02 09:05:03 -0800

    Silence sync

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ rm -rf /tmp/${LAMBCI_REPO}
 git clone --depth 1 https://github.com/${LAMBCI_REPO} /tmp/${LAMBCI_REPO}
 cd /tmp/${LAMBCI_REPO}
 
-aws s3 sync  /tmp/${LAMBCI_REPO} s3://${ORIGIN_BUCKET} --delete --exclude ".git/*"
+pip install --user awscli
+aws s3 sync  /tmp/${LAMBCI_REPO} s3://${ORIGIN_BUCKET} --delete --exclude ".git/*" --quiet
