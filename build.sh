29bfff0c865dd67488444b9b66929d514a63672d
limed <limed@mozilla.com>
2017-03-02 08:50:26 -0800

    Attempt to build tlscanary with lambci

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+
+export LAMBCI_REPO="mwobensmith/ssl_compat"
+export ORIGIN_BUCKET="limed-test-stage"
+
+rm -rf /tmp/${LAMBCI_REPO}
+git clone --depth 1 https://github.com/${LAMBCI_REPO} /tmp/${LAMBCI_REPO}
+cd /tmp/${LAMBCI_REPO}
+
+aws s3 sync  /tmp/${LAMBCI_REPO} s3://${ORIGIN_BUCKET} --delete --ignore ".git/*"
